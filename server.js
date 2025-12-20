033d7b669a528e6c7f19602d6afe01d69ed44319
pergawhatsapp <victor@perga-vmc.com>
2025-12-20 13:12:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,38 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const twilio = require('twilio');
 const { handleMessage } = require('./bot/stateHandler');
-
-const PRODUCTS = [
-  {
-    id: 1,
-    key: 'PERGA_COLA',
-    name_en: 'Perga Cola',
-    name_es: 'Perga Cola',
-    price: 21.6
-  },
-  {
-    id: 2,
-    key: 'PERGA_ORANGE',
-    name_en: 'Perga Orange',
-    name_es: 'Perga Naranja',
-    price: 21.6
-  },
-  {
-    id: 3,
-    key: 'PERGA_LIME',
-    name_en: 'Perga Lime',
-    name_es: 'Perga Limón-Lima',
-    price: 21.6
-  },
-  {
-    id: 4,
-    key: 'MALTA',
-    name_en: 'Malta Perga',
-    name_es: 'Malta Perga',
-    price: 21.6
-  }
-];
-
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
@@ -44,9 +12,6 @@ app.post('/webhook', async (req, res) => {
   const response = await handleMessage(from, incomingMsg, req);
   res.set('Content-Type', 'text/xml');
   res.send(response);
-
-  state.temp_data = state.temp_data || {};
-  state.order_items = state.order_items || {};
 });
 
 app.listen(process.env.PORT, () => {
