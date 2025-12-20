75852765bad5286913dfad0a35ddd2814251d9bf
pergawhatsapp <victor@perga-vmc.com>
2025-12-20 13:05:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ const express = require('express');
 const twilio = require('twilio');
 const { handleMessage } = require('./bot/stateHandler');
 
+const PRODUCTS = [
+  {
+    id: 1,
+    key: 'PERGA_COLA',
+    name_en: 'Perga Cola',
+    name_es: 'Perga Cola',
+    price: 21.6
+  },
+  {
+    id: 2,
+    key: 'PERGA_ORANGE',
+    name_en: 'Perga Orange',
+    name_es: 'Perga Naranja',
+    price: 21.6
+  },
+  {
+    id: 3,
+    key: 'PERGA_LIME',
+    name_en: 'Perga Lime',
+    name_es: 'Perga Limón-Lima',
+    price: 21.6
+  },
+  {
+    id: 4,
+    key: 'MALTA',
+    name_en: 'Malta Perga',
+    name_es: 'Malta Perga',
+    price: 21.6
+  }
+];
+
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
