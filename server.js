e9c02aadbe759104844820f22cbafc0ddff78ac8
pergawhatsapp <victor@perga-vmc.com>
2025-12-21 01:58:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,3 +19,8 @@ app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
+const express = require('express');
+const path = require('path');
+
+app.use('/tmp', express.static(path.join(__dirname, 'tmp')));
+});
